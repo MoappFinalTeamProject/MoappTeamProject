ad727d132dce9dc3b6d3970988cab8f2bbd7b3d2
21900768CCH <21900768@handong.ac.kr>
2023-11-23 01:57:34 +0900

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:moapp_team_project/auth/login.dart';
 import 'package:moapp_team_project/auth/register.dart';
-import 'package:moapp_team_project/pages/chat_page/chat_ui_page.dart';
-import 'package:moapp_team_project/pages/info_page/info.dart';
+import 'package:moapp_team_project/pages/feed_page/add_feed_page.dart';
 import 'package:moapp_team_project/pages/navigation.dart';
 
 class FinalApp extends StatelessWidget {
@@ -15,10 +14,10 @@ class FinalApp extends StatelessWidget {
       initialRoute: '/login',
       debugShowCheckedModeBanner: false,
       routes: {
-        '/': (BuildContext context) => const NavigationPage(),
+         '/': (BuildContext context) => const NavigationPage(),
         '/login': (BuildContext context) => const MyLoginPage(),
-        '/info': (BuildContext context) => const MyBasicInfoPage(),
         '/register': (BuildContext context) => const MyRegisterPage(),
+        '/addFeed': (BuildContext context) => const AddFeedPage(),
       },
       theme: ThemeData.light(useMaterial3: true),
     );
