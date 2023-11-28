05239867886cbc4dd284a7f3946b4170fabf4947
uone0628 <121482693+uone0628@users.noreply.github.com>
2023-11-28 10:58:26 +0900

    [feat] my preference

--- lib/pages/profile_page/my_preference.dart
+++ lib/pages/profile_page/my_preference.dart
@@ -19,6 +19,7 @@ class _MyPreferenceState extends State<MyPreference> {
     '요리': false,
     '노래 부르기': false,
     '노래방 가기': false,
+    '잠': false,
   };
 
   Map<String, bool> foodCategories = {
@@ -26,8 +27,82 @@ class _MyPreferenceState extends State<MyPreference> {
     '중식': false,
     '일식': false,
     '양식': false,
-    '베트남식': false,
+    '면류': false,
+    '밥류': false,
+    '빵류': false,
+    '국물류': false,
+    '볶음류': false,
+    '튀김류': false,
   };
+
+  Map<String, bool> majorCategories = {
+    '경영경제': false,
+    '커뮤니케이션': false,
+    '콘텐츠디자인': false,
+    '생명': false,
+    '기계': false,
+    '전산전자': false,
+    'ICT창업': false,
+    '법': false,
+    '상담심리사회복지': false,
+    '국제어문': false,
+    '공간환경시스템': false,
+  };
+
+  Map<String, bool> regionCategories = {
+    '서울': false,
+    '경기': false,
+    '인천': false,
+    '대전': false,
+    '강원': false,
+    '충북': false,
+    '충남': false,
+    '전북': false,
+    '전남': false,
+    '경북': false,
+    '경남': false,
+    '대구': false,
+    '울산': false,
+    '부산': false,
+    '광주': false,
+    '제주': false,
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    loadPreferences();
+  }
+
+  void loadPreferences() async {
+    final doc = await FirebaseFirestore.instance
+        .collection('member')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection('member info')
+        .doc('preference')
+        .get();
+
+    if (doc.exists) {
+      setState(() {
+        hobbyCategories = {
+          ...hobbyCategories,
+          ...Map<String, bool>.from(doc['hobby'])
+        };
+        foodCategories = {
+          ...foodCategories,
+          ...Map<String, bool>.from(doc['food'])
+        };
+        majorCategories = {
+          ...majorCategories,
+          ...Map<String, bool>.from(doc['major'])
+        };
+        regionCategories = {
+          ...regionCategories,
+          ...Map<String, bool>.from(doc['region'])
+        };
+      });
+    }
+  }
 
   void savePreferences() {
     FirebaseFirestore.instance
@@ -38,6 +113,8 @@ class _MyPreferenceState extends State<MyPreference> {
         .set({
       'hobby': hobbyCategories,
       'food': foodCategories,
+      'major': majorCategories,
+      'region': regionCategories,
     });
   }
 
@@ -90,6 +167,8 @@ class _MyPreferenceState extends State<MyPreference> {
           children: [
             buildCategory('취미', hobbyCategories),
             buildCategory('음식', foodCategories),
+            buildCategory('전공', majorCategories),
+            buildCategory('지역', regionCategories),
           ],
         ),
       ),
